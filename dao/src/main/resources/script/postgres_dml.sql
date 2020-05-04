d373a6d8bfd18d8c284df8666097c48bdcc4891f
IllIdanIllI <ilya_galica@mail.ru>
2020-05-04 15:04:58 +0300

    Added dml script

---
+++ dao/src/main/resources/script/postgres_dml.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.sensor (id, name, model, range, location, description, unit, type) VALUES (1, 'name', 'model', '[1,10)', 'dfdfs', 'very big enormous description', 'voltage', 'Pressure');
+INSERT INTO public.sensor (id, name, model, range, location, description, unit, type) VALUES (8, 'dname3', 'cool3', '[13,15)', 'here3', 'very good3', 'voltage', 'Pressure');
+INSERT INTO public.sensor (id, name, model, range, location, description, unit, type) VALUES (9, 'dname4', 'cool4', '[14,16)', 'here4', 'very good4', 'voltage', 'Pressure');
+INSERT INTO public.sensor (id, name, model, range, location, description, unit, type) VALUES (7, 'dname2', 'cool2', '[12,16)', 'here2', 'very good2', 'voltage', 'Pressure');
+INSERT INTO public.sensor (id, name, model, range, location, description, unit, type) VALUES (2, 'dname', 'coolllll', '[10,13)', 'here', 'very good', 'voltage', 'Pressure');
+INSERT INTO public.sensor (id, name, model, range, location, description, unit, type) VALUES (4, 'dname1', 'cool1', '[11,17)', 'here1', 'very good1', 'voltage', 'Pressure');
+
+
+
+INSERT INTO public."user" (id, login, password, role) VALUES (1, 'first', 'first', 'VIEWER');
+INSERT INTO public."user" (id, login, password, role) VALUES (2, 'admin', '$2a$10$E0MlOsqi/IT/stUh.CwKHOhh/K.56PSExY4MG5dbwNM88iWizabHS', 'ADMINISTRATOR');
+INSERT INTO public."user" (id, login, password, role) VALUES (3, 'commonViewer', '$2a$10$E/gebckvr7ZqwgGpA03AuOlAi4s9.e9SgOWrFYjYqZXoL8tVa0mUq', 'VIEWER');
+
+
+
